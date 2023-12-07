cbc7bd2fd2f0e30325f0144b1bb9139af7da1fc4
JabulaniSifundza <jjsifundza@gmail.com>
2023-12-07 19:54:00 +0200

    ML Packages

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ import requests
 from langchain.embeddings.google_palm import GooglePalmEmbeddings
 from langchain.llms import GooglePalm
 from langchain.chains.question_answering import load_qa_chain
+import datetime as dt
+import statsmodels.api as sm
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt import risk_models
+from pypfopt import expected_returns
+import math
+import keras
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler
+import pandas_datareader.data as web
 
 app = Flask(__name__)
 
