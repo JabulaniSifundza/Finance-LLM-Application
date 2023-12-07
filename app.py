f2e2bf19fef183aeccc63921fc33b2239b56da75
JabulaniSifundza <jjsifundza@gmail.com>
2023-12-07 19:21:35 +0200

    Chance Render

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+import pandas as pd
+import numpy as np
+import google.generativeai as palm
+import yfinance as yf
+import requests
+import chromadb
+from chromadb.api.types import Documents, Embeddings
 
 app = Flask(__name__)
 
